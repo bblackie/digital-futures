1a01fae3ea0d83c031241148e6b5aea0b225a671
bblackie <brian.blackie@trinityschools.nz>
2026-04-12 12:16:04 +1200

    Returned baseful to df

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://docs.digitalfutures.nz',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/digital-futures/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
