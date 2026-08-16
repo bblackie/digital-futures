9a5dcfcef475661ccd1cf1d999cef040b994d46e
Brian James Blackie <122497534+bblackie@users.noreply.github.com>
2026-08-17 07:47:39 +1200

    updated readme and Y9 game listing

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -3,8 +3,9 @@ export default function ImageGallery({ images }) {
   return (
     <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))', gap: '16px' }}>
       {images.map((img) => (
-        <a key={img.src} href={img.www}>
+        <a key={img.src} href={img.www} style={{ textAlign: 'center' }}>
           <img src={img.src} alt={img.alt} style={{ width: '100%' }} />
+          <div>{`Sir ${img.alt}`}</div>
         </a>
       ))}
     </div>
